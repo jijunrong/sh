305fecd667680c148a2443928cda3e1b7363d1c1
jijunrong <98357998+jijunrong@users.noreply.github.com>
2023-06-04 05:27:42 +0800

    Update rmsnell.sh

--- rmsnell.sh
+++ rmsnell.sh
@@ -3,9 +3,5 @@
 systemctl stop snell.service
 systemctl disable snell.service
 rm -f /etc/systemd/system/snell.service
-
-cd
-ARCHITECTURE=$(dpkg --print-architecture)
-rm -f snell-server-v4.0.1-linux-$ARCHITECTURE.zip
-rm -f snell-server
-rm -f snell-server.conf
+rm -f /usr/local/bin/snell-server
+rm -f /etc/snell/snell-server.conf
