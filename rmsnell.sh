ae157d0489b5a7ace492467d4838ab9c1e104541
jijunrong <98357998+jijunrong@users.noreply.github.com>
2023-06-04 06:13:44 +0800

    Update rmsnell.sh

--- rmsnell.sh
+++ rmsnell.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#停止进程。
 systemctl stop snell.service
+#取消开机启动进程。
 systemctl disable snell.service
-rm -f /etc/systemd/system/snell.service
-rm -f /usr/local/bin/snell-server
-rm -f /etc/snell/snell-server.conf
+#删除snell守护进程。
+rm -rf /etc/systemd/system/snell.service
+#删除snell程序。
+rm -rf /usr/local/bin/snell-server
+#删除snell配置文件夹。
+rm -rf /etc/snell/
